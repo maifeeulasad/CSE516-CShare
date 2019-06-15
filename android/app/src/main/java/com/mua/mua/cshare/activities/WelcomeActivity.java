852075fa7c8f6debc8bdacd8928765a66c04688e
Avisheak Das Shomrat <avisheakdasshomrat510@gmail.com>
2019-06-16 01:14:52 +0600

    Removed merge conflict

--- android/app/src/main/java/com/mua/mua/cshare/activities/WelcomeActivity.java
+++ android/app/src/main/java/com/mua/mua/cshare/activities/WelcomeActivity.java
@@ -1,14 +1,9 @@
 package com.mua.mua.cshare.activities;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-=======
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
->>>>>>> 8af3bdd6c183dafb6dc5e84ef4fcb37b9c0d6128
 
 import com.mua.mua.cshare.R;
 
